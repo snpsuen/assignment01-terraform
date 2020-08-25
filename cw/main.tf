17082a6b32537cec3945853f5be70797fe296560
snpsuen <66404197+snpsuen@users.noreply.github.com>
2020-08-25 19:37:20 +0800

    Update main.tf

--- cw/main.tf
+++ cw/main.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_log_group" "assignment01-ecs" {
   name = "assignment01-ecs-logging"
 
   tags = {
-    Environment = "hypothetical"
-    Application = "assignment01"
+    Name = "${var.name}-cw-${var.environment}"
+    Environment = var.environment
   }
 }
